86c0092e26925fef0a1debfe3c56ccbffe131b21
findeevy <135389114+findeevy@users.noreply.github.com>
2023-09-22 13:51:42 -0500

    Update CPU-Predictor.py

--- CPU-Predictor.py
+++ CPU-Predictor.py
@@ -4,7 +4,7 @@ import statistics
 def readWords(filename='cpu.dat'):
     return([i[0:i.index("\n")].split(", ") for i in list(open(filename))])
 
-#Read in the CPU information database.
+#Read in the CPU information database, split the CPU info at each comma.
 cpuList = readWords()
 
 #Get user's CPU information.
@@ -12,11 +12,11 @@ userCPUName = input("What is your CPU's name?")
 userCPUSpeed = float(input("What is your CPU's speed in GHz?"))
 userCPUCore = int(input("What is your CPU's core count?"))
 
-#Match the user's CPU core count.
+#Match the user's CPU core count to a list of similar CPUs.
 closestCoreCount=min([float(i[1]) for i in cpuList], key=lambda i:abs(i-userCPUCore))
 coreMatch=[i for i in cpuList if float(i[1])==closestCoreCount]
 
-#Match the user's CPU speed.
+#Match the user's CPU clock speed to CPUs with similar corecount.
 closestSpeed=min([float(i[2]) for i in coreMatch], key=lambda i:abs(i-userCPUSpeed))
 matchList=[i for i in coreMatch if float(i[2])==closestSpeed]
 matchNames=[i[0] for i in coreMatch if float(i[2])==closestSpeed]
@@ -24,5 +24,5 @@ matchNames=[i[0] for i in coreMatch if float(i[2])==closestSpeed]
 #Predict the user's CPU's Cinebench score by averaging the matched CPU(s) scores and calculating the difference in clock speed and core count.
 predictedCinebench=statistics.mean([int(i[3]) for i in matchList])*(userCPUSpeed/closestSpeed)*(userCPUCore/closestCoreCount)
 
-#Print the results.
+#Print the results to the user.
 print("Your processor ("+userCPUName+") has a predicted Cinebench R20 Multithreaded score of "+str(int(predictedCinebench))+" and is most similar to the following CPU(s):\n"+"\n".join(matchNames))
